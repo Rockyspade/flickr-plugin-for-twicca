e6c026fbdea3fb14583df84455c535832ec567f8
dmp <devnull@localhost>
2010-05-16 23:55:03 +0900

    twiccaの設定画面およびアップロード画面に登場するようになった

--- src/net/itsuha/flickr_twicca/activity/SettingActivity.java
+++ src/net/itsuha/flickr_twicca/activity/SettingActivity.java
@@ -1,9 +1,11 @@
-package net.itsuha.flickr_twicca;
+package net.itsuha.flickr_twicca.activity;
 
+import net.itsuha.flickr_twicca.R;
+import net.itsuha.flickr_twicca.R.layout;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class FlickrTwiccaActivity extends Activity {
+public class SettingActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
---
+++ src/net/itsuha/flickr_twicca/activity/UploadActivity.java
@@ -0,0 +1,13 @@
+package net.itsuha.flickr_twicca.activity;
+
+import net.itsuha.flickr_twicca.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UploadActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
